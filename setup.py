6bf6e1f5ba461faf75c9abf57800d17a0298255b
Finn <finnmiddletonbaird@gmail.com>
2022-02-02 15:50:58 +0000

    Added setup.py and changed #! to python

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = "RLTrainingEnv",
+    version = "0.0.1",
+    url = "https://github.com/finn1y/RLTraingingEnv",
+    author = "Finn Middleton-Baird",
+    install_requirements=["gym", "pygame", "numpy", "tensorflow", "matplotlib"]
+)
