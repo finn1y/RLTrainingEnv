c52fbc51a7a5b665be0311655a95d8bc64bb6cba
Finn <finnmiddletonbaird@gmail.com>
2022-02-10 17:50:30 +0000

    updated setup.py with new structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(
-    name = "RLTrainingEnv",
+    name = "rl_training_env",
     version = "0.0.1",
     url = "https://github.com/finn1y/RLTraingingEnv",
     author = "Finn Middleton-Baird",
